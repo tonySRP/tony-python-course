b1ea13adaa927d968667f1b00d27da6894351ee5
tonySRP <tony@skyrockprojects.com>
2018-07-23 14:59:16 +0800

    complete exercises

--- 0-introduction/2-functions.py
+++ 0-introduction/2-functions.py
@@ -1,8 +1,21 @@
-# write your own function that takes two numbers and subtracs them
+# write your own function that takes two numbers and subtracts them
+def subtract(a,b):
+    return a - b
 
+threeminustwo = subtract(10,5)
+print(threeminustwo)
 # WRITE YOUR SUBTRACTION FUNCTION HERE (call it subtract)
 
+ihatethis = threeminustwo
+
 # also write a function that prints out it's input twice
+def printTwice(ihatethis):
+    print(ihatethis)
+    print(ihatethis)
+
+
+    
+printTwice(threeminustwo)
 
 # WRITE YOUR FUNTION THAT PRINTS TWICE HERE (call it printTwice)
 
@@ -20,16 +33,16 @@
 
 # These are test cases we will learn about them next lesson
 #DO NOT EDIT THIS
-def checkOutput(index, value):
-    if(output[index] == value):
-        return "CORRECT!"
-    return "WRONG, try again"
+#def checkOutput(index, value):
+#    if(output[index] == value):
+#        return "CORRECT!"
+#    return "WRONG, try again"
 
-firstInput = [1,2,3,4,5]
-secondInput = [-1,0,14,22,3]
-output = [2,2,-11,-18,2]
-for i in range(5):
-    print(firstInput[i]," + ",secondInput[i]," = ",subtract(firstInput[i],secondInput[i])," ",checkOutput(i,subtract(firstInput[i],secondInput[i])))
-for i in firstInput:
-    printTwice(i)
+#firstInput = [1,2,3,4,5]
+#secondInput = [-1,0,14,22,3]
+#output = [2,2,-11,-18,2]
+#for i in range(5):
+#    print(firstInput[i]," + ",secondInput[i]," = ",subtract(firstInput[i],secondInput[i])," ",checkOutput(i,subtract(firstInput[i],secondInput[i])))
+#for i in firstInput:
+#    printTwice(i)
 
--- 0-introduction/3-test-cases.py
+++ 0-introduction/3-test-cases.py
@@ -7,3 +7,14 @@ def testTripleString(word):
     return word+word+word
 
 #add some test cases for both of them
+
+def printTwice(input):
+    print(input)
+    print(input)
+
+base = 10
+word = 'corn'
+
+printTwice(testCube(base))
+printTwice(testTripleString(word))
+
